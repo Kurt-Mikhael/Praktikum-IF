6a3b3c9219450b90071a715c05a5eac2bc078e45
Kurt-Mikhael <kurtmikhael123@gmail.com>
2025-04-30 23:20:55 +0700

    Prakpraktikum 6

---
+++ Alpro 1/Praktikum 6/Prapraktikum/Boolean.h
@@ -0,0 +1,10 @@
+/* File: Boolean.h */
+/* Definisi tipe data Boolean dan konstanta TRUE/FALSE */
+
+#ifndef BOOLEAN_H
+#define BOOLEAN_H
+
+/* Definisi tipe Boolean */
+typedef enum { false = 0, true = 1 } boolean;
+
+#endif
